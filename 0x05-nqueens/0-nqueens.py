0bdad0300c23ce9673febce53ba68eef7af0e8cb
iamqaasim <iamqaasim@gmail.com>
2023-04-26 11:55:25 -0700

    My first commit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,89 +3,55 @@
 
 import sys
 
+def print_board(board):
+    for row in board:
+        print("".join(row))
 
-if __name__ == '__main__':
+def is_safe(board, row, col, n):
+    # Check the row on the left side
+    for i in range(col):
+        if board[row][i] == "Q":
+            return False
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == "Q":
+            return False
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, n), range(col, -1, -1)):
+        if board[i][j] == "Q":
+            return False
+    return True
+
+def solve(board, col, n):
+    # If all queens are placed, print the solution
+    if col == n:
+        print_board(board)
+        print()
+        return True
+    # Place queen in every row of current column and check if it's safe
+    res = False
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = "Q"
+            res = solve(board, col + 1, n) or res
+            board[i][col] = "."
+    return res
+
+def n_queens(n):
+    # Initialize the board with dots
+    board = [["." for x in range(n)] for y in range(n)]
+    solve(board, 0, n)
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-
+        sys.exit(1)
     try:
         n = int(sys.argv[1])
     except ValueError:
-        print('N must be a number')
-        exit(1)
-
+        print("N must be a number")
+        sys.exit(1)
     if n < 4:
-        print('N must be at least 4')
-        exit(1)
-
-    solutions = []
-    placed_queens = []  # coordinates format [row, column]
-    stop = False
-    r = 0
-    c = 0
-
-    # iterate thru rows
-    while r < n:
-        goback = False
-        # iterate thru columns
-        while c < n:
-            # check is current column is safe
-            safe = True
-            for cord in placed_queens:
-                col = cord[1]
-                if(col == c or col + (r-cord[0]) == c or
-                        col - (r-cord[0]) == c):
-                    safe = False
-                    break
-
-            if not safe:
-                if c == n - 1:
-                    goback = True
-                    break
-                c += 1
-                continue
-
-            # place queen
-            cords = [r, c]
-            placed_queens.append(cords)
-            # if last row, append solution and reset all to last unfinished row
-            # and last safe column in that row
-            if r == n - 1:
-                solutions.append(placed_queens[:])
-                for cord in placed_queens:
-                    if cord[1] < n - 1:
-                        r = cord[0]
-                        c = cord[1]
-                for i in range(n - r):
-                    placed_queens.pop()
-                if r == n - 1 and c == n - 1:
-                    placed_queens = []
-                    stop = True
-                r -= 1
-                c += 1
-            else:
-                c = 0
-            break
-        if stop:
-            break
-        # on fail: go back to previous row
-        # and continue from last safe column + 1
-        if goback:
-            r -= 1
-            while r >= 0:
-                c = placed_queens[r][1] + 1
-                del placed_queens[r]  # delete previous queen coordinates
-                if c < n:
-                    break
-                r -= 1
-            if r < 0:
-                break
-            continue
-        r += 1
-
-    for idx, val in enumerate(solutions):
-        if idx == len(solutions) - 1:
-            print(val, end='')
-        else:
-            print(val)
+        print("N must be at least 4")
+        sys.exit(1)
+    n_queens(n)
